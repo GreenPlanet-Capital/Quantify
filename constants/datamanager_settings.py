6405b9a79d43605ec66da5db329070e6cc2dad91
Atharva Kale <atharvakale343@gmail.com>
2022-02-12 16:48:24 -0500

    Changes

--- constants/datamanager_settings.py
+++ constants/datamanager_settings.py
@@ -4,4 +4,4 @@ def setup_datamgr_settings():
 
     # Set env variable to absolute path of datamanager folder
     os.environ['DATAMGR_ABS_PATH'] = os.path.join(os.getcwd(), 'DataManager')
-    print(os.environ['DATAMGR_ABS_PATH'])
+    assert os.path.isdir(os.environ['DATAMGR_ABS_PATH']), "DataManger is not a part of the current folder"
