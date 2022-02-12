9153486a07318e47255b02ebd2e1199d19db337e
Atharva Kale <atharvakale343@gmail.com>
2022-02-11 23:38:00 -0500

    Added lookback mapping

--- constants/strategy_defs.py
+++ constants/strategy_defs.py
@@ -15,14 +15,20 @@ def get_strategy_definitons():
     }
 
     strat_id_to_TimeFrame = {
-        0: TimeFrame(27, '1Day')
+        0: TimeFrame(100, '1Day')
     }
 
     assert len(strat_name_to_id)==len(strat_id_to_TimeFrame), "Lengths Mismatch: Add a strategy to both strat_name_to_id and strat_id_to_TimeFrame"
 
+    strat_id_to_lookback = {
+        0: 100
+    }
+
+    assert len(strat_name_to_id)==len(strat_id_to_TimeFrame), "Lengths Mismatch: Add a strategy to both strat_name_to_id and strat_id_to_lookback"
+
     class Strategies:
-        def macd_rsi(self):
-            return Macd_Rsi()
+        def macd_rsi(self, timeframe: TimeFrame, lookback):
+            return Macd_Rsi(timeframe, lookback)
 
     assert len(strat_name_to_id) == \
         len(inspect.getmembers(Strategies, inspect.isfunction)), "Lengths Mismatch: Add a strategy to both strat_name_to_id and the Strategies class"
@@ -30,7 +36,7 @@ def get_strategy_definitons():
     strats = Strategies()
 
     strat_id_to_class = { 
-        id: getattr(strats, name.lower())(strat_id_to_TimeFrame[id])
+        id: getattr(strats, name.lower())(strat_id_to_TimeFrame[id], strat_id_to_lookback[id])
         for name, id in strat_name_to_id.items()
     }
 
