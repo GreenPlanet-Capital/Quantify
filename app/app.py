54e10e60a320e8b8991273ea5c04f9285d45c001
Atharva Kale <atharvakale343@gmail.com>
2022-02-11 23:37:45 -0500

    Resolved TODO

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 import os, sys
+sys.path.append(os.getcwd())
 from typing import List
 
 from constants.strategy_defs import get_strategy_definitons
 from opportunity.opportunity import Opportunity
-sys.path.append(os.getcwd())
 
 from strats.base_strategy import BaseStrategy
 
@@ -34,7 +34,7 @@ def setup_data():
     # Now import DataManager
     from DataManager.datamgr import data_manager
     this_manager = data_manager.DataManager(limit=10, update_before=False, exchangeName = 'NYSE', isDelisted=False)
-    dict_of_dfs = this_manager.get_stock_data('2019-05-01 00:00:00', 
+    dict_of_dfs = this_manager.get_stock_data('2019-01-01 00:00:00', 
                                             '2019-06-01 00:00:00',
                                             api='Alpaca')
     list_of_final_symbols = this_manager.list_of_symbols
@@ -44,7 +44,6 @@ def main():
     setup_strategies()
     setup_data()
     strat: BaseStrategy = strat_id_to_class[strat_name_to_id['MACD_RSI']]
-    # TODO Ensure dataframe has enough entries
     strat.set_data(list_of_tickers=list_of_final_symbols, dict_of_dataframes=dict_of_dfs)
     opps: List[Opportunity] = strat.run()
     print()
