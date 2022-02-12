01d5a16cd6b121fa3b2090d6bc40a3ef71c11f99
Atharva Kale <atharvakale343@gmail.com>
2022-02-11 23:38:15 -0500

    utility functions

---
+++ constants/utils.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def get_normalized_value(x, lower_bound, upper_bound, max_x, min_x):
+    return ((upper_bound-lower_bound)*(x - min_x))/(max_x - min_x) + lower_bound
+
+def normalize_values(series: pd.Series, lower_bound, upper_bound) -> pd.Series:
+    min_x = series.min()
+    max_x = series.max()
+
+    args = (lower_bound, upper_bound, max_x, min_x)
+    series_out = series.apply(get_normalized_value, args=args)
+    return series_out
