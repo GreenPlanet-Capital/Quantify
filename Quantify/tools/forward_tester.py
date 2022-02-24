7503ce19beed982c8547f8cd9164d91e1e459a08
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2022-02-23 22:45:49 -0500

    Improved speed of forward tester

--- Quantify/tools/forward_tester.py
+++ Quantify/tools/forward_tester.py
@@ -2,6 +2,7 @@ from typing import List, Dict, Tuple
 from pandas import DataFrame
 from DataManager.utils.timehandler import TimeHandler
 import pandas as pd
+from tqdm import tqdm
 from Quantify.constants.utils import find_loc
 from Quantify.positions.opportunity import Opportunity
 from Quantify.positions.position import Position
@@ -11,7 +12,7 @@ from Quantify.tools.base_tester import BaseTester
 
 class ForwardTester(BaseTester):
     def __init__(self, list_of_final_symbols: List[str], dict_of_dfs: Dict[str, DataFrame], exchangeName: str,
-                 strat: BaseStrategy, num_top: int, percent_l: float=0.7):
+                 strat: BaseStrategy, num_top: int, percent_l=0.7):
         super().__init__(list_of_final_symbols, dict_of_dfs, exchangeName, strat, num_top, percent_l)
 
     def execute_strat(self, graph_positions=False, print_terminal=False) -> List[Position]:
@@ -50,22 +51,19 @@ class ForwardTester(BaseTester):
         return positions
 
     def advance_forward(self, start_index, end_index, positions) -> Tuple[Dict[str, DataFrame], Dict[str, DataFrame]]:
-        current_dict_dfs, dict_score_dfs = dict(), dict()
+        current_dict_dfs, dict_score_dfs = self.dict_of_dfs, dict()
+        self.strat.set_data(list_of_tickers=self.list_of_final_symbols,
+                            dict_of_dataframes=current_dict_dfs,
+                            exchangeName=self.exchangeName)
+        dict_score_dfs = self.strat.multiple_health_check(positions)
 
-        for i in range(start_index, end_index + 1):
-            current_dict_dfs = {k: v[:i] for k, v in self.dict_of_dfs.items()}
-            self.strat.set_data(list_of_tickers=self.list_of_final_symbols,
-                                dict_of_dataframes=current_dict_dfs,
-                                exchangeName=self.exchangeName)
-
-            dict_score_dfs = self.strat.multiple_health_check(positions)
-
+        for i in tqdm(range(start_index, end_index + 1), desc=f'Forward Testing for {end_index + 1} days'):
             any_is_active = False
             for ticker, tuple_df_pos in dict_score_dfs.items():
                 score_df, this_pos = tuple_df_pos
-                if not score_df.empty and score_df['exit_signal'].iloc[-1] and this_pos.is_active:
+                if not score_df.empty and score_df['exit_signal'].iloc[i] and this_pos.is_active:
                     this_pos.is_active = False
-                    current_df = current_dict_dfs[ticker].iloc[-1]
+                    current_df = current_dict_dfs[ticker].iloc[i]
                     exit_timestamp, exit_price = TimeHandler.get_clean_datetime_from_string(current_df['timestamp']), \
                                                  current_df['close']
 
@@ -73,11 +71,10 @@ class ForwardTester(BaseTester):
                     this_pos.exit_price = exit_price
                 any_is_active = any_is_active or this_pos.is_active
 
-            self.strat.zero_data()
-
-            if (not any_is_active):
+            if not any_is_active:
                 break
 
+        self.strat.zero_data()
         return current_dict_dfs, dict_score_dfs
 
     def handle_remaining_positons(self, current_dict_dfs, positions):
@@ -87,7 +84,6 @@ class ForwardTester(BaseTester):
                 current_df = current_dict_dfs[pos.ticker].iloc[-1]
                 exit_timestamp, exit_price = TimeHandler.get_clean_datetime_from_string(current_df['timestamp']), \
                                              current_df['close']
-
                 pos.exit_timestamp = exit_timestamp
                 pos.exit_price = exit_price
 
@@ -107,8 +103,8 @@ class ForwardTester(BaseTester):
             list_dates = [this_pos.timestamp, this_pos.exit_timestamp]
             list_dates = list(map(TimeHandler.get_string_from_datetime, list_dates))
 
-            to_graph['graph_score'] = to_graph['score'] # * 10 + to_graph['close'].mean()
-            to_graph['graph_health_score'] = to_graph['health_score'] # * 10 + to_graph['close'].mean()
+            to_graph['graph_score'] = to_graph['score'] * 10 + to_graph['close'].mean()
+            to_graph['graph_health_score'] = to_graph['health_score'] * 10 + to_graph['close'].mean()
 
             fig = to_graph.plot(x='timestamp', y=[to_graph['graph_score'], to_graph['close'],
                                                   to_graph['graph_health_score']],
@@ -117,14 +113,14 @@ class ForwardTester(BaseTester):
             to_graph['cleaned_timestamp'] = to_graph['timestamp'].apply(TimeHandler.get_clean_string_from_string)
             list_locs = find_loc(to_graph, list_dates)
 
-            self.add_graph_annotations(fig, to_graph.loc[list_locs[0]], this_pos)
-            self.add_graph_annotations(fig, to_graph.loc[list_locs[1]], this_pos)
+            self.add_graph_annotations(fig, to_graph.loc[list_locs[0]], this_pos, 'Enter')
+            self.add_graph_annotations(fig, to_graph.loc[list_locs[1]], this_pos, 'Exit')
             fig.update_layout(showlegend=False)
             fig.show()
 
-    def add_graph_annotations(self, input_fig, list_locs, curr_position):
+    def add_graph_annotations(self, input_fig, list_locs, curr_position, position_do):
         input_fig.add_annotation(x=list_locs['timestamp'].iloc[0],
                                  y=list_locs['close'].iloc[0],
-                                 text=f"Enter date (type: {curr_position.order_type})",
+                                 text=f"{position_do} date (type: {curr_position.order_type})",
                                  showarrow=True,
                                  arrowhead=1)
