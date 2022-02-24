5c52d2b20b26d9c81c6c531a63e1aecfbdf059dd
Atharva Kale <atharvakale343@gmail.com>
2022-02-24 00:39:22 -0500

    reworked rsi logic, made it lighter

--- Quantify/indicators/rsi.py
+++ Quantify/indicators/rsi.py
@@ -1,7 +1,8 @@
+import numpy as np
 from pandas import DataFrame
 from Quantify.constants.utils import normalize_values
 from Quantify.indicators.base_indicator import BaseIndicator
-
+import pandas as pd
 
 class Rsi(BaseIndicator):
     def __init__(self, window_length=14) -> None:
@@ -14,48 +15,21 @@ class Rsi(BaseIndicator):
         assert len(input_dataframe[
                        'close']) >= self.window_length + 1, "Not enough entries in input dataframe to calculate RSI"
 
-        rsi_df = DataFrame()
+        delta = input_dataframe["close"].diff()
 
-        # Calculate Price Differences
-        rsi_df['diff'] = input_dataframe['close'].diff(1)
+        up = delta.copy()
+        up[up < 0] = 0
+        up = pd.Series.ewm(up, alpha=1/self.window_length).mean()
 
-        # Calculate Avg. Gains/Losses
-        rsi_df['gain'] = rsi_df['diff'].clip(lower=0).round(2)
-        rsi_df['loss'] = rsi_df['diff'].clip(upper=0).abs().round(2)
+        down = delta.copy()
+        down[down > 0] = 0
+        down *= -1
+        down = pd.Series.ewm(down, alpha=1/self.window_length).mean()
 
-        # Get initial Averages
-        rsi_df['avg_gain'] = rsi_df['gain'].rolling(window=self.window_length,
-                                                    min_periods=self.window_length).mean()[:self.window_length + 1]
-        rsi_df['avg_loss'] = rsi_df['loss'].rolling(window=self.window_length,
-                                                    min_periods=self.window_length).mean()[:self.window_length + 1]
-
-        # TODO Optimize dataframe operations
-
-        # Get WMS averages
-        # Average Gains
-        for i, row in enumerate(rsi_df['avg_gain'].iloc[self.window_length + 1:]):
-            rsi_df['avg_gain'].iloc[i + self.window_length + 1] = \
-                (rsi_df['avg_gain'].iloc[i + self.window_length] *
-                 (self.window_length - 1) +
-                 rsi_df['gain'].iloc[i + self.window_length + 1]) \
-                / self.window_length
-
-        # Average Losses
-        for i, row in enumerate(rsi_df['avg_loss'].iloc[self.window_length + 1:]):
-            rsi_df['avg_loss'].iloc[i + self.window_length + 1] = \
-                (rsi_df['avg_loss'].iloc[i + self.window_length] *
-                 (self.window_length - 1) +
-                 rsi_df['loss'].iloc[i + self.window_length + 1]) \
-                / self.window_length
-
-        # Calculate RS Values
-        rsi_df['rs'] = rsi_df['avg_gain'] / rsi_df['avg_loss']
-
-        # Calculate RSI
-        rsi_df['rsi'] = 100 - (100 / (1.0 + rsi_df['rs']))
+        rsi = np.where(up == 0, 0, np.where(down == 0, 100, 100 - (100 / (1 + up / down))))
 
         to_return_rsi_df = DataFrame()
-        to_return_rsi_df['rsi'] = rsi_df['rsi']
+        to_return_rsi_df['rsi'] = rsi
 
         to_return_rsi_df['normalized rsi'] = normalize_values(normalize_values(to_return_rsi_df['rsi'], -1, 1).abs(), 0,
                                                               1)
