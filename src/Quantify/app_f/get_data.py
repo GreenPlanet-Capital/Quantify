463ba4a5dc5a243d7a0c95072923fb410659a38d
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2022-07-24 01:11:09 -0400

    bug fixes & stop loss

--- src/Quantify/app_f/get_data.py
+++ src/Quantify/app_f/get_data.py
@@ -5,38 +5,46 @@ from DataManager.utils.timehandler import TimeHandler
 start_timestamp = datetime(2021, 7, 6)
 end_timestamp = datetime(2022, 7, 21)
 exchangeName = "NASDAQ"
-limit = 5
+limit = None
 update_before = False
 n_best = 5
 percent_l = 0.5
 
 
-def get_data():
+def get_data(list_specific_stocks=None):
     return setup_data(
         start_timestamp=start_timestamp,
         end_timestamp=end_timestamp,
         limit=limit,
         exchangeName=exchangeName,
         update_before=update_before,
+        list_specific_stocks=list_specific_stocks
     )
 
 
 def setup_data(
-    start_timestamp: datetime,
-    end_timestamp: datetime,
-    limit,
-    exchangeName,
-    update_before,
+        start_timestamp: datetime,
+        end_timestamp: datetime,
+        limit,
+        exchangeName,
+        update_before,
+        list_specific_stocks=None
 ):
-
     this_manager = data_manager.DataManager(
         limit=limit, update_before=update_before, exchangeName=exchangeName
     )
 
     start_timestamp = TimeHandler.get_string_from_datetime(start_timestamp)
     end_timestamp = TimeHandler.get_string_from_datetime(end_timestamp)
-    dict_of_dfs = this_manager.get_stock_data(
-        start_timestamp, end_timestamp, api="Alpaca", fetch_data=False
-    )
+
+    dict_of_dfs = dict()
+    if list_specific_stocks is None:
+        dict_of_dfs = this_manager.get_stock_data(
+            start_timestamp, end_timestamp, api="Alpaca", fetch_data=False
+        )
+    else:
+        for stock in list_specific_stocks:
+            dict_of_dfs[stock] = this_manager._daily_stocks.get_specific_stock_data(stock, start_timestamp, end_timestamp)
+
     list_of_final_symbols = this_manager.list_of_symbols
     return list_of_final_symbols, dict_of_dfs
--- src/Quantify/app_f/p_backtesting.py
+++ src/Quantify/app_f/p_backtesting.py
@@ -5,14 +5,15 @@ import pandas as pd
 import ta
 import numpy as np
 from Quantify.app_f.get_data import *
-from Quantify.constants.utils import buy_sell_mva, normalize_values
+from Quantify.constants.utils import buy_sell_mva, normalize_values, buy_sell_rsi
+from tqdm import tqdm
 
 
-class MacdRsi(Strategy):
+class MacdRsiBoll(Strategy):
     def init(self, window_length=14, lower_ma=12, upper_ma=26, signal_length=9):
         price = self.data.Close.s
 
-        self.rsi = pd.Series(self.I(ta.momentum.rsi, price, window_length, plot=False))
+        self.rsi = pd.Series(self.I(ta.momentum.rsi, price, window_length))
         self.rsi_normalized = normalize_values(
             normalize_values(self.rsi, -1, 1).abs(),
             0,
@@ -34,7 +35,7 @@ class MacdRsi(Strategy):
             1,
         )
 
-        self.macd = pd.Series(self.I(ta.trend.macd, price, window_length))
+        self.macd = pd.Series((self.I(ta.trend.macd, price, window_length, plot=False)))
 
         self.normalized_macd = normalize_values(
             normalize_values(self.macd, -1, 1).abs(),
@@ -42,22 +43,26 @@ class MacdRsi(Strategy):
             1,
         )
 
-        self.upper_boll = pd.Series(self.I(ta.volatility.bollinger_hband, price, window_length, plot=False))
-        self.lower_boll = pd.Series(self.I(ta.volatility.bollinger_lband, price, window_length, plot=False))
-        self.diff_boll = 1 - normalize_values(
-            self.upper_boll - self.lower_boll, 0, 1
+        self.upper_boll = pd.Series(
+            self.I(ta.volatility.bollinger_hband, price, window_length, plot=False)
+        )
+        self.lower_boll = pd.Series(
+            self.I(ta.volatility.bollinger_lband, price, window_length, plot=False)
+        )
+        self.diff_boll = 1 - normalize_values(self.upper_boll - self.lower_boll, 0, 1)
+
+        self.buy_sell_signal = self.I(
+            lambda: self.rsi.apply(buy_sell_rsi), name="Buy/Sell Signal", plot=False
         )
 
-        self.buy_sell_signal = self.macd.apply(buy_sell_mva)
-
         self.rsi_score = np.where(
-            self.buy_sell_signal == 1, 1 - self.rsi / 100, self.rsi
+            pd.Series(self.buy_sell_signal) == 1, 1 - self.rsi / 100, self.rsi
         )
 
         self.pre_score = (
-                0.50 * self.rsi_score
-                + 0.35 * self.macd_diff * self.normalized_macd
-                + 0.15 * self.diff_boll
+                0.30 * self.rsi_normalized
+                + 0.35 * (self.macd_diff * self.normalized_macd) ** 0.5
+                + 0.35 * self.diff_boll
         )
         self.pre_score = normalize_values(
             self.pre_score,
@@ -66,7 +71,7 @@ class MacdRsi(Strategy):
         )
         self.score = self.I(lambda: self.pre_score, name="Score")
 
-        self.health_score = 0.60 * self.rsi_score + 0.40 * (
+        self.health_score = 0.60 * self.rsi_normalized + 0.40 * (
                 1 - self.normalized_macd.replace({0: np.nan})
         ).fillna(0)
         self.health_score = normalize_values(
@@ -75,35 +80,84 @@ class MacdRsi(Strategy):
             1,
         )
         self.health_score = self.I(lambda: self.health_score, name="Health Score")
-        self.high_score = np.full(shape=len(self.data), fill_value=0.90)
-        self.low_score = np.full(shape=len(self.data), fill_value=0.10)
+        self.high_score = self.I(
+            lambda: np.full(shape=len(self.data), fill_value=0.70),
+            name="High Score",
+            plot=False,
+        )
+        self.low_score = self.I(
+            lambda: np.full(shape=len(self.data), fill_value=0.30),
+            name="Low Score",
+            plot=False,
+        )
 
     def next(self):
         if crossover(self.score, self.high_score) and not self.position.is_long:
-            if self.buy_sell_signal.iloc[-1] == 1:
-                print("buy")
-                self.buy(size=10)
+            if self.buy_sell_signal[-1] == 1:
+                # print("buy")
+                self.buy(size=self.score[-1], sl=0.075)
             else:
-                print("sell")
-                self.sell(size=10)
-        elif self.position.size != 0 and crossover(self.low_score, self.health_score):
-            print("close")
-            self.trades[-1].close()
+                # print("sell")
+                self.sell(size=self.score[-1], sl=0.075, limit=0.065)
+
+        elif self.position.size != 0 and self.health_score[-1] < self.low_score[-1]:
+            # print("close")
+            for t in self.trades:
+                t.close()
+
+        for t in self.trades:
+            health_series = pd.Series(self.health_score[t.entry_bar:])
+            current_peak_change_health = health_series.cummax()
+            trailing_stop_health = current_peak_change_health * (1 - 0.30)
+            exit_signal = health_series < trailing_stop_health
+
+            if exit_signal.iloc[-1]:
+                # print("close top loss")
+                t.close()
 
 
 if __name__ == "__main__":
     list_of_final_symbols, dict_of_dfs = get_data()
+    list_all_bt = []
+    total_equity_return = 0
 
-    print(f"{list_of_final_symbols=}")
+    for ticker, TICKER_DF in tqdm(dict_of_dfs.items()):
+        TICKER_DF = TICKER_DF.rename(
+            columns={
+                "open": "Open",
+                "high": "High",
+                "low": "Low",
+                "close": "Close",
+                "volume": "Volume",
+            }
+        )
 
-    ticker, TICKER_DF = list(dict_of_dfs.items())[0]  # pick one stock
+        TICKER_DF = TICKER_DF.set_index(pd.to_datetime(TICKER_DF["timestamp"])).drop(
+            "timestamp", axis=1
+        )
 
-    TICKER_DF = TICKER_DF.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low',
-                                          'close': 'Close', 'volume': 'Volume'})
+        bt = Backtest(TICKER_DF, MacdRsiBoll, commission=0.002, exclusive_orders=True)
+        stats = bt.run()
 
-    TICKER_DF = TICKER_DF.set_index(pd.to_datetime(TICKER_DF['timestamp'])).drop('timestamp', axis=1)
+        cur_return = stats["Equity Final [$]"] - 10000
+        list_all_bt.append((cur_return, bt, ticker))
+        total_equity_return += cur_return
 
-    bt = Backtest(TICKER_DF, MacdRsi, commission=0.002, exclusive_orders=True)
-    stats = bt.run()
-    # print(stats)
-    bt.plot()
+    list_all_bt.sort(reverse=True, key=lambda d: d[0])
+    top_n = 5
+
+    print([obj[2] for obj in list_all_bt[:top_n]])
+    print([obj[2] for obj in list_all_bt[-top_n:]])
+
+    for stat_return, bt_return, ticker_return in list_all_bt[:top_n]:
+        bt_return.plot(open_browser=False, filename=f"graph_files/{ticker_return}")
+
+    for stat_return, bt_return, ticker_return in list_all_bt[-top_n:]:
+        bt_return.plot(open_browser=False, filename=f"graph_files/{ticker_return}")
+
+    print(f"{total_equity_return=}")
+    print(f"{round(total_equity_return / (len(list_all_bt) * 10000) * 100, 2)}")
+
+    # bt = Backtest(GOOG, MacdRsiBoll, commission=0.002, exclusive_orders=True)
+    # bt.run()
+    # bt.plot(open_browser=False, filename=f"graph_files/GOOG")
