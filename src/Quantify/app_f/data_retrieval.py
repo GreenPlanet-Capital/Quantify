399735234aeb22f4de3523d4946dfdd90752054b
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2023-02-06 16:58:18 -0500

    Minor fix

--- src/Quantify/app_f/data_retrieval.py
+++ src/Quantify/app_f/data_retrieval.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from DataManager.datamgr import data_manager
+from DataManager.utils.timehandler import TimeHandler
+
 
 def get_specific_data(list_specific_stocks=None, fetch_data=True):
-    start_timestamp = datetime(2021, 7, 6)
-    end_timestamp = datetime(2022, 7, 21)
+    start_timestamp = datetime(2022, 8, 1)
+    end_timestamp = datetime(2023, 2, 2)
     exchangeName = "NASDAQ"
     limit = None
     update_before = False
@@ -34,12 +36,12 @@ def get_data(
 
     dict_of_dfs = dict()
     if list_specific_stocks is None:
+        start_dt, end_dt = map(TimeHandler.get_string_from_datetime, [start_timestamp, end_timestamp])
         dict_of_dfs = this_manager.get_stock_data(
-            start_timestamp, end_timestamp, api="Alpaca", fetch_data=fetch_data
+            start_dt, end_dt, api="Alpaca", fetch_data=fetch_data
         )
     else:
         for stock in list_specific_stocks:
-            import pdb;pdb.set_trace()
             dict_of_dfs[stock] = this_manager._daily_stocks.get_specific_stock_data(stock, start_timestamp, end_timestamp)
 
     list_of_final_symbols = this_manager.list_of_symbols
